3eda051f4b285c86d5987469b1200481ffc28621
ZaRamen <rlin7289@gmail.com>
2022-12-18 17:46:56 -0500

    added all bugs

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     let bugs = document.getElementsByClassName('bugs');
     var wrapper = document.getElementsByClassName('wrapper')[0];
-    var bugSize = 200;
-    var wrapperWidth = getComputedStyle(wrapper).width.split('px')[0];
-    var wrapperHeight = getComputedStyle(wrapper).height.split('px')[0];
-    var dx = 2;
-    var dy = -2;
+    var bugSize = 100;
+    var wrapperWidth = parseInt(getComputedStyle(wrapper).width.split('px')[0], 10);
+    var wrapperHeight = parseInt(getComputedStyle(wrapper).height.split('px')[0], 10);
+    var dx = 20;
+    var dy = -20;
     var bugsList = [];
+
     var i = 0;
     for (let bug of bugs) {
 
@@ -16,16 +17,27 @@ document.addEventListener("DOMContentLoaded", function (event) {
             splat(this);
 
         });
+       
         let bugObject = {
-            x: (wrapperWidth / 2),
+            x: wrapperWidth / 2,
             y: wrapperHeight - 200,
             index: i
         }
+        if (i >= 1)
+        {
+            bugObject = {
+                x: (wrapperWidth / 2) - (Math.floor(Math.random() * 700)),
+                y: bugsList[i - 1].y - (Math.floor(Math.random() * 20 + 30)),
+                index: i
+            }
+        }
         bugsList.push(bugObject);
-        console.log(bugsList[bugObject.index].x)
+        // console.log(bugsList[bugObject.index].x)
         moveBug(bug, bugObject.index);
         i++;
     }
+   
+    console.log(bugsList)
 
     function splat(ele) {
         console.log('splat')
@@ -34,23 +46,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
     function moveBug(ele, index) { 
     
         let timer = setInterval(() => {
+          
+            translate(ele, bugsList[index]);
            
-            translate(ele, bugsList[index].x, bugsList[index].y);
-            console.log(bugsList[index].x)
         }, 100)
     }
-    function translate(ele, x, y) {
-        if (x + dx > wrapperWidth - bugSize || x + dx < bugSize) {
+    function translate(ele, bug) {
+        if (bug.x + dx > wrapperWidth - bugSize || bug.x + dx < bugSize) {
            
             dx = -dx;
         }
-        if (y + dy > wrapperHeight - bugSize || y + dy <= -50) {
+        if (bug.y + dy > wrapperHeight - bugSize || bug.y + dy <= -50) {
             dy = -dy;
         }
-        x += dx;
-        y += dy;
-        ele.style.left = x + "px";
-        ele.style.top = y + "px";
+        bug.x += dx;
+        bug.y += dy;
+        ele.style.left = bug.x + "px";
+        ele.style.top = bug.y + "px";
         
     }
 });
