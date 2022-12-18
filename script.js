c6f1894d2becc3d83588312c4fbf6f400e9f9bc9
ZaRamen <indead991@gmail.com>
2022-12-18 16:39:55 -0500

    added animation

--- script.js
+++ script.js
@@ -1,83 +1,51 @@
 
 
-function splat(ele)
-{
-    console.log('splat')
-    ele.style.display = "none";
-}
-function setBugPosition(ele)
-{
-
-}
-
-function moveBug(ele)
-{
-    let i = 0;
-    let direction = false;
-    let timer = setInterval(() =>{
-        
-        
-        translate(ele, moveDirection(), moveDirection())
-   
-        
-        // limit to 70% of the page
-        // if (i < 70 && !direction)
-        // {
-        //     i+=1;
-        // }
-        // else
-        // {
-        //     direction = true;
-        //     i += -1;
-        //     if (i <= 0)
-        //     {
-        //         direction = false;
-        //     }
-        // }
-       
-        
-    }, 50)
-}
-function translate( ele, x, y )
- {
-    var left = ele.style.left,
-        top = ele.style.top,
-        dx = left - x,
-        dy = top - y,
-        i = 1,
-        count = 50,
-        delay = 20;
-    
-    function loop() 
-    {
-        if ( i >= count ) { return; }
-        i += 1;
-        ele.style.left = ( left - ( dx * i / count ) ).toFixed(0) + '%';
-        ele.style.top = ( top - ( dy * i / count ) ).toFixed(0) + '%';
-        setTimeout( loop, delay );
-    }
-    
-    loop();
-}
-function moveDirection()
-{
-
-    return Math.floor(Math.random() * 70);
-}
-
-document.addEventListener("DOMContentLoaded", function(event)
-{
+document.addEventListener("DOMContentLoaded", function (event) {
     let bugs = document.getElementsByClassName('bugs');
-    console.log(bugs)
-    for (let bug of bugs)
-    {
-        bug.addEventListener('click', function (event)
-        {
+    var wrapper = document.getElementsByClassName('wrapper')[0];
+    var bugSize = 200;
+    var wrapperWidth = getComputedStyle(wrapper).width.split('px')[0];
+    var wrapperHeight = getComputedStyle(wrapper).height.split('px')[0];
+    var x = wrapperWidth / 2;
+    var y = wrapperHeight - 200;
+    var dx = 30;
+    var dy = -10;
+    console.log(wrapperHeight)
+    console.log(x)
+
+
+    for (let bug of bugs) {
+        bug.addEventListener('click', function (event) {
             splat(this);
-            
+
         });
 
         moveBug(bug);
+    }
+
+    function splat(ele) {
+        console.log('splat')
+        ele.style.display = "none";
+    }
+    function moveBug(ele) {
+        let timer = setInterval(() => {
+            translate(ele)
+
+        }, 100)
+    }
+    function translate(ele) {
+
+
+        if (x + dx > wrapperWidth - bugSize || x + dx < bugSize) {
+            dx = -dx;
+        }
+        if (y + dy > wrapperHeight - bugSize || y + dy <= -50) {
+            dy = -dy;
+        }
+        x += dx;
+        y += dy;
+        ele.style.left = x + "px";
+        ele.style.top = y + "px";
     }
 });
 
